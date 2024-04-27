66df0b477bd7180ca852ca7625d753b3ddf1bf2b
ibadmore <122062584+ibadmore@users.noreply.github.com>
2024-04-27 06:00:08 -0700

    Update faceanalysis.py

--- faceanalysis.py
+++ faceanalysis.py
@@ -1,14 +1,23 @@
-IS_INSIGHTFACE_INSTALLED = False
+IS_DLIB_INSTALLED = True
+try:
+    import dlib
+    IS_DLIB_INSTALLED = True
+except ImportError:
+    pass
+
+IS_INSIGHTFACE_INSTALLED = True
 try:
     from insightface.app import FaceAnalysis
     IS_INSIGHTFACE_INSTALLED = True
 except ImportError:
     pass
 
-if not IS_INSIGHTFACE_INSTALLED:
-    raise Exception("Please install insightface to use this node.")
+if not IS_DLIB_INSTALLED and not IS_INSIGHTFACE_INSTALLED:
+    raise Exception("Please install either dlib or insightface to use this node.")
 
 INSTALLED_LIBRARIES = []
+if IS_DLIB_INSTALLED:
+    INSTALLED_LIBRARIES.append("dlib")
 if IS_INSIGHTFACE_INSTALLED:
     INSTALLED_LIBRARIES.append("insightface")
 
@@ -147,3 +156,127 @@ class FaceBoundingBox:
             #out_img = torch.stack(out_img)
         
         return (out_img, out_x, out_y, out_w, out_h,)
+
+class FaceEmbedDistance:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "analysis_models": ("ANALYSIS_MODELS", ),
+                "reference": ("IMAGE", ),
+                "image": ("IMAGE", ),
+                "filter_thresh_eucl": ("FLOAT", { "default": 1.0, "min": 0.001, "max": 2.0, "step": 0.001 }),
+                "filter_thresh_cos": ("FLOAT", { "default": 1.0, "min": 0.001, "max": 2.0, "step": 0.001 }),
+                "filter_best": ("INT", { "default": 0, "min": 0, "max": 4096, "step": 1 }),
+                "generate_image_overlay": ("BOOLEAN", { "default": True }),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE", "FLOAT", "FLOAT")
+    RETURN_NAMES = ("IMAGE", "euclidean", "cosine")
+    OUTPUT_IS_LIST = (False, True, True)
+    FUNCTION = "analize"
+    CATEGORY = "FaceAnalysis"
+
+    def analize(self, analysis_models, reference, image, filter_thresh_eucl=1.0, filter_thresh_cos=1.0, filter_best=0, generate_image_overlay=True):
+        if generate_image_overlay:
+            font = ImageFont.truetype(os.path.join(os.path.dirname(os.path.realpath(__file__)), "Inconsolata.otf"), 32)
+            background_color = ImageColor.getrgb("#000000AA")
+            txt_height = font.getmask("Q").getbbox()[3] + font.getmetrics()[1]
+
+        self.analysis_models = analysis_models
+
+        ref = []
+        for i in reference:
+            ref_emb = self.get_descriptor(np.array(T.ToPILImage()(i.permute(2, 0, 1).cpu()).convert('RGB')))
+            if ref_emb is not None:
+                ref.append(torch.from_numpy(ref_emb))
+        
+        if ref == []:
+            raise Exception('No face detected in reference image')
+
+        ref = torch.stack(ref)
+        ref = np.array(torch.mean(ref, dim=0))
+
+        out = []
+        out_eucl = []
+        out_cos = []
+        
+        for i in image:
+            img = np.array(T.ToPILImage()(i.permute(2, 0, 1).cpu()).convert('RGB'))
+
+            img = self.get_descriptor(img)
+
+            if img is None: # No face detected
+                eucl_dist = 1.0
+                cos_dist = 1.0
+            else:
+                if np.array_equal(ref, img): # Same face
+                    eucl_dist = 0.0
+                    cos_dist = 0.0
+                else:
+                    eucl_dist = np.float64(np.linalg.norm(ref - img))
+                    cos_dist = 1 - np.dot(ref, img) / (np.linalg.norm(ref) * np.linalg.norm(img))
+            
+            if eucl_dist <= filter_thresh_eucl and cos_dist <= filter_thresh_cos:
+                print(f"\033[96mFace Analysis: Euclidean: {eucl_dist}, Cosine: {cos_dist}\033[0m")
+
+                if generate_image_overlay:
+                    tmp = T.ToPILImage()(i.permute(2, 0, 1)).convert('RGBA')
+                    txt = Image.new('RGBA', (image.shape[2], txt_height), color=background_color)
+                    draw = ImageDraw.Draw(txt)
+                    draw.text((0, 0), f"EUC: {round(eucl_dist, 3)} | COS: {round(cos_dist, 3)}", font=font, fill=(255, 255, 255, 255))
+                    composite = Image.new('RGBA', tmp.size)
+                    composite.paste(txt, (0, tmp.height - txt.height))
+                    composite = Image.alpha_composite(tmp, composite)
+                    out.append(T.ToTensor()(composite).permute(1, 2, 0))
+                else:
+                    out.append(i)
+
+                out_eucl.append(eucl_dist)
+                out_cos.append(cos_dist)
+
+        if not out:
+            raise Exception('No image matches the filter criteria.')
+
+        # filter out the best matches
+        if filter_best > 0:
+            out = np.array(out)
+            out_eucl = np.array(out_eucl)
+            out_cos = np.array(out_cos)
+            idx = np.argsort((out_eucl + out_cos) / 2)
+            out = torch.from_numpy(out[idx][:filter_best])
+            out_eucl = out_eucl[idx][:filter_best].tolist()
+            out_cos = out_cos[idx][:filter_best].tolist()
+
+        if isinstance(out, list):
+            out = torch.stack(out)
+
+        return(out, out_eucl, out_cos,)
+    
+    def get_descriptor(self, image):
+        embeds = None
+
+        if self.analysis_models["library"] == "insightface":
+            faces = self.analysis_models["detector"].get(image)
+            if len(faces) > 0:
+                embeds = faces[0].normed_embedding
+        else:
+            faces = self.analysis_models["detector"](image)
+            if len(faces) > 0:
+                shape = self.analysis_models["shape_predict"](image, faces[0])
+                embeds = np.array(self.analysis_models["face_recog"].compute_face_descriptor(image, shape))
+
+        return embeds
+
+NODE_CLASS_MAPPINGS = {
+    "FaceEmbedDistance": FaceEmbedDistance,
+    "FaceAnalysisModels": FaceAnalysisModels,
+    "FaceBoundingBox": FaceBoundingBox,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "FaceEmbedDistance": "Face Embeds Distance",
+    "FaceAnalysisModels": "Face Analysis Models",
+    "FaceBoundingBox": "Face Bounding Box",
+}
